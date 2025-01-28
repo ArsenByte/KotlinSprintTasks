355bafe8c6f25d35936cc18f0d7c3f41962fb049
ArsenByte <arseniyshakov30@gmail.com>
2025-01-29 00:28:38 +0300

    KS-1-1  Create variables for count of orders and text with thanks

---
+++ src/main/kotlin/Lesson_1/task1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson_1
+
+fun main() {
+    var countOfOrders: Int = 75
+    val textWithThanks: String = "Большое спасибо за покупку!"
+
+    println("Количество заказов: " + countOfOrders)
+    println(textWithThanks)
+}
