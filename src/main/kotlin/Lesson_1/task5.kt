42030b7154bc4e7b4c405738f941fe76d5faa277
ArsenByte <arseniyshakov30@gmail.com>
2025-01-29 02:14:21 +0300

    KS-1-4  Task Space

---
+++ src/main/kotlin/Lesson_1/task5.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson_1
+
+fun main() {
+    var seconds = 6480
+    var minutes = 6480 / 60
+    seconds %= 60
+    val hours = minutes / 60
+    minutes %= 60
+    print(0)
+    print(hours)
+    print(":")
+    print(minutes)
+    print(":")
+    print(0)
+    print(seconds)
+
+}
