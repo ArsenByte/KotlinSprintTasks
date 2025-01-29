f9e4433d0fb3d31588463936f4efdab6c59afcc6
ArsenByte <arseniyshakov30@gmail.com>
2025-01-30 00:25:19 +0300

    KS-2-1  Find average score of 4 students

---
+++ src/main/kotlin/lesson2/task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson2
+
+fun main() {
+
+    val markOfStudent1 = 3
+    val markOfStudent2 = 4
+    val markOfStudent3 = 3
+    val markOfStudent4 = 5
+
+    val averageResult = (markOfStudent1 + markOfStudent2 + markOfStudent3 + markOfStudent4) / 4.0f
+
+    println(averageResult)
+}
