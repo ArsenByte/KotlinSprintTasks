c77283f5debf065ade03fd4c1013e9fbf9856577
ArsenByte <arseniyshakov30@gmail.com>
2025-01-31 01:38:30 +0300

    KS-2-1 Create new integer variable for number of students

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -2,12 +2,13 @@ package org.example.lesson2
 
 fun main() {
 
+    val numberOfStudents = 4
     val markOfStudent1 = 3
     val markOfStudent2 = 4
     val markOfStudent3 = 3
     val markOfStudent4 = 5
 
-    val averageResult = (markOfStudent1 + markOfStudent2 + markOfStudent3 + markOfStudent4) / 4.0f
+    val averageResult = (markOfStudent1 + markOfStudent2 + markOfStudent3 + markOfStudent4) / numberOfStudents.toFloat()
 
     println(averageResult)
 }
