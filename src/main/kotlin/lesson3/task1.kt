5ed4db7c56830e8df4ee114b0b3793ac9bd2bf3d
ArsenByte <arseniyshakov30@gmail.com>
2025-02-04 00:54:16 +0300

    KS-3-1 Create greeting text for day and night.

---
+++ src/main/kotlin/lesson3/task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson3
+
+fun main() {
+
+    val name = "Arsen"
+    val textDay = "Good morning!"
+    val textNight = "Good night!"
+
+    var greeting = "$textDay $name"
+    println(greeting)
+
+    greeting = "$textNight $name"
+    println(greeting)
+
+}
